7e7ef8edb0d7b2870b8b30bb5f497e4963f01622
oluwaetosin <oluwaetosin@gmail.com>
2016-06-23 10:42:19 +0100

    added api

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var app = express();
     app.use(bodyParser.urlencoded({extended:true}));
     app.use(bodyParser.json());
     app.use(morgan('dev'));
-    var api = require("./app/routes/api");
+    var api = require("./app/routes/api")(app,express);
     
     app.use('/api',api);
     app.get('*',function(req,res){
